d1f2bdea57d7fa391f8ce9b605949a8776602462
noahedmonds <edmonds.noah@vashonsd.org>
2018-03-26 08:13:52 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/company/Book.java
    #	src/com/company/Main.java

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,17 +1,21 @@
 package com.company;
 
-public class Book extends Item{
+public class Book extends Item {
 
-    public Book(){
+    public Book() {
         super("Book", "A super boring book written by Karl Marx and Friedrich Engels", false);
     }
-        @Override
-        public String handle (String s) {
-        if(s.equalsIgnoreCase("examine")){
+
+    @Override
+    public String handle(String s) {
+        if (s.equalsIgnoreCase("examine")) {
             return this.getDescription();
         }
-        else{
-        return "I don;t know how to" + s + "a book.";
+        if (s.equalsIgnoreCase("vandalise")) {
+            this.setDescription("A book containing something");
+        } else {
+            return "I don't know how to" + s + "a book.";
         }
-}
+        return null;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,45 +13,50 @@ public class Main {
         System.out.println(curRoom.listItemsInRoom());
 
         // offer help, since this is the player's first time playing
-        String prompt = input.readLine(curRoom.getDescription()+ " Type your command. If you need help, type 'help.'");
+        String prompt = input.readLine(curRoom.getDescription() + " Type your command. If you need help, type 'help.'");
 
-        while(true) {
+        while (true) {
             String arr[] = prompt.split(" ", 2);
-            String verb = arr[0];  //This is the throwaway word
-            String directObject = arr[1];
-            if(curRoom.hasItem(directObject)) {
-                Item whichItem = curRoom.getItem(directObject);
-                String response = whichItem.handle(verb);
-                System.out.println(response);
-            } else if(curRoom.hasRoom(verb)) {
-                curRoom = curRoom.getRoom(verb);
-                System.out.println(curRoom.getDescription());
-                System.out.print("In this room, there is:");
-                System.out.print(curRoom.listItemsInRoom());
-            }
-            else if (verb.equalsIgnoreCase("look")) {
-                System.out.println(curRoom.getDescription());
-            }
-            else if (verb.equalsIgnoreCase("quit")) {
-                System.out.println("Are you sure you want to quit? (y/n)");
-                String quitPrompt = input.readLine();
-                if(quitPrompt.equalsIgnoreCase("y")) {
-                    System.out.println("Thanks for playing!");
-                    break;
+            if (arr.length < 2) {
+                if (arr[0].equalsIgnoreCase("help")) {
+                    listOfCommands();
                 }
-                else {
+            } else {
+                String verb = arr[0];  //This is the throwaway word
+                String directObject = arr[1];
+
+
+                if (curRoom.hasItem(directObject)) {
+                    Item whichItem = curRoom.getItem(directObject);
+                    String response = whichItem.handle(verb);
+                    System.out.println(response);
+
+                } else if (curRoom.hasRoom(verb)) {
+                    curRoom = curRoom.getRoom(verb);
                     System.out.println(curRoom.getDescription());
+                    System.out.print("In this room, there is:");
+                    System.out.print(curRoom.listItemsInRoom());
+                } else if (verb.equalsIgnoreCase("look")) {
+                    System.out.println(curRoom.getDescription());
+                } else if (verb.equalsIgnoreCase("quit")) {
+                    System.out.println("Are you sure you want to quit? (y/n)");
+                    String quitPrompt = input.readLine();
+                    if (quitPrompt.equalsIgnoreCase("y")) {
+                        System.out.println("Thanks for playing!");
+                        break;
+                    } else {
+                        System.out.println(curRoom.getDescription());
+                    }
+                } else if (verb.equalsIgnoreCase("help")) {
+                    System.out.println(listOfCommands());
+                } else {
+                    System.out.println("You can't do that ");
                 }
             }
-            else if (verb.equalsIgnoreCase("help")){
-                System.out.println(listOfCommands());
+                prompt = input.readLine();
             }
-            else{
-                System.out.println("You can't do that ");
-            }
-            prompt = input.readLine();
         }
-    }
+
 
     //This is a utility method to set up all the rooms and their connections.
     //Returns the main Room.
@@ -80,12 +85,13 @@ public class Main {
         return home;
     }
     public static String listOfCommands() {
-            return ("Here's a list of useful commands: \nThe 'Look' command will " +
+            System.out.println ("Here's a list of useful commands: \nThe 'Look' command will " +
                     "give you a description of your surroundings. \nThe 'Examine' command will give you a description of an" +
                     "object. \nThe 'Inventory' command will give you a list of the items in your inventory and a description of" +
                     " them. \nThe 'Restart' command will restart the game \nThe 'Quit' command will quit the game for you. Make " +
                     "sure you save before you quit!");
 
 
+        return null;
     }
 }
